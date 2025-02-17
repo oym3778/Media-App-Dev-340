7471e21766af33391ad8a0b4443112eb1d0eeeee
oym3778 <oym3778@g.rit.edu>
2025-02-17 11:53:54 -0500

    added a statefulWidget

--- flutter_design_to_spec/lib/main.dart
+++ flutter_design_to_spec/lib/main.dart
@@ -10,11 +10,34 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trees & Wood"),
+        backgroundColor: Color(0x35353D00), // TO-DO Fix the color
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Hello World!')
+          ],
         ),
       ),
+      
     );
   }
 }
