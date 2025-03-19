36a31e8638cdf1a4be101460310364e459dfc99a
oym3778 <oym3778@g.rit.edu>
2025-03-18 23:16:41 -0400

    initial commit tali fitness

---
+++ project2/talli_fitness/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: MainPage(),
+      ),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tali Fitness"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Column(
+        children: [
+          Text("data")
+        ],
+      ),
+    );
+  }
+}
